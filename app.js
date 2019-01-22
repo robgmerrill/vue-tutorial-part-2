1fc8c1a87d44cd30af5fad242655a607db98e867
Rob Merrill <robgmerrill@gmail.com>
2019-01-22 11:41:01 -0800

    Setup new section

---
+++ app.js
@@ -0,0 +1,9 @@
+const meal = new Vue({
+    el: '#meal',
+    data: {
+      meal: 'Thanksgiving Meal',
+      meat: 'Turkey',
+      vegetable: 'Green Beans',
+      starch: 'Mashed Potatoes',
+    }
+});
